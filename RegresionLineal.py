77adde5ce37310d03f5f1115898b998ebac44ddc
Liam Salazar <salazar.martinez.liamantonio@gmail.com>
2025-04-30 14:55:54 -0600

    commit

--- RegresionLineal.py
+++ RegresionLineal.py
@@ -39,22 +39,36 @@ def pendiente(x:list,y:list):
     m = (suma_productos - n*prom_x*prom_y)/(suma_cuadrados-n*(prom_x)**2)
     return m
 
-def pendiente_ordenada_origen(x:list, y:list):
+def ordenada_origen(x:list, y:list):
     b = calcularPromedio(y) - pendiente(x,y)*calcularPromedio(x)
     return b
 
 # Datos de prueba
 horas = [2,4,6,8,10,12,14,16,18,20,
-        22,1,25,5,7,9,11,13,15,17]
+        5,7,9,11,13,15,17]
 promedios = [5.1,5.8,6.4,6.0,8.0,7.5,9.1,8.7,9.4,9.5,
-            7.4,8.5,7.9,6.0,6.6,7.5,7.9,8.5,8.8,9.2]
+            6.0,6.6,7.5,7.9,8.5,8.8,9.2]
 
-# Crear una recta
+# Crear una recta ejemplo
 y_modelo = []
 for i in range(len(horas)):
     y = (1/4)*horas[i] + 4.5
     y_modelo.append(y)
 
+# Crear recta real
+m = pendiente(horas, promedios)
+b = ordenada_origen(horas, promedios)
+def estimaciones(x:list, y:list):
+    y_estimado = []
+    for i in range(len(x)):
+        Y = pendiente(x,y)*x[i] + ordenada_origen(x,y)
+        y_estimado.append(Y)
+    return y_estimado
+
+estimados = estimaciones(horas, promedios)
+
+print("M: ", m)
+print("B: ", b)
 print(correlacion(horas, promedios))
 print(calcularDesviacionEstandar(horas))
 print(calcularDesviacionEstandar(promedios))
@@ -64,7 +78,8 @@ plt.scatter(horas, promedios) # Datos en ejes (x,y)
 plt.xlabel("Horas") # Título en Y
 plt.ylabel("Promedios") # Título en X
 plt.title("Promedio según horas de estudio") # Título general
-plt.plot(horas, y_modelo) # Graficar recta
+#plt.plot(horas, y_modelo) # Graficar recta ejemplo
+plt.plot(horas, estimados) # Graficar recta de regresion
 plt.show() # Mostrar gráfica
 
 # -1 = Linea de regresion perfecta de pendiente negativa
