08b36f08f14ffd45c073da9369ae56921c5909da
Liam Salazar <salazar.martinez.liamantonio@gmail.com>
2025-04-30 14:26:24 -0600

    commit

--- RegresionLineal.py
+++ RegresionLineal.py
@@ -50,12 +50,11 @@ plt.title("Promedio según horas de estudio") # Título general
 plt.plot(horas, y_modelo) # Graficar recta
 plt.show() # Mostrar gráfica
 
-# Graficación de recta
-plt
-
 # -1 = Linea de regresion perfecta de pendiente negativa
 # 0 = Datos totalmente dispersos, no aplica regresión lineal
 # 1 = Datos de regresion perfecta de pendiente positiva
 # distancia (Error) = Y real - Y estimado
 # Sumatoria de error al cuadrado, aumenta los errores para enfocarse en ellos y no en los
 # pequeños, a esto se le llama error cuadrático.
+# b = promedio(y)-m*promedio(x)
+# m = (Suma(x*y)-n*promedio(x)*promedio(y))/(Suma(x**2)-nx**2)
