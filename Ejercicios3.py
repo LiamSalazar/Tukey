b7df1e3a5c6ba71818fa3d2ff7571f9fb90179dc
Liam Salazar <salazar.martinez.liamantonio@gmail.com>
2025-05-09 09:00:20 -0600

    parentesis

--- Ejercicios3.py
+++ Ejercicios3.py
@@ -21,8 +21,39 @@ def aproximacionLeibniz(n):
     print(sum)
     return
 
+def area(nums):
+    maxArea = -1000
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            base = abs(nums[j]-nums[i])
+            altura = min(nums[i], nums[j])
+            currentArea = base * altura
+            if currentArea >= maxArea:
+                maxArea = currentArea
+    print(maxArea)
+    return
 
-nums = [2,3,7,9,11]
-sumNums(nums, 16)
-print("APROXIMACION PI:")
-aproximacionLeibniz(100000)
+def parentesis(cadena):
+    stack = []
+    parentesis = "([{}])"
+    diccionario = {")":"(", "]":"[", "}":"{"}
+    for letra in cadena:
+        if letra in parentesis:
+            if letra in diccionario:
+                top = stack.pop() if stack else '#'
+                if top != diccionario[letra]:
+                    return False
+            else:
+                stack.append(letra)
+    return not stack
+
+
+#nums = [2,3,7,9,11]
+#sumNums(nums, 16)
+#print("APROXIMACION PI:")
+#aproximacionLeibniz(100)
+#print("AREA")
+#nums2 = [15,20,30,10,5]
+#area(nums2)
+p = parentesis("([])")
+print(p)
