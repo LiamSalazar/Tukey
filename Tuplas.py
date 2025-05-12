ebed568998e78a8fdea7a09ff54bd37b7a8878e0
Liam Salazar <salazar.martinez.liamantonio@gmail.com>
2025-05-12 14:44:53 -0600

    commit

--- Tuplas.py
+++ Tuplas.py
@@ -27,3 +27,33 @@ def cuadrado(lista):
 print(media(1,2,3,4,5,6))
 print(funcion(10, 5))
 print(cuadrado([1,2,3,4,5]))
+
+#l1 = [1,2,3]
+#l2 = [4,5,6]
+#l3 = l1
+#l4 = l1[:]
+#print(id(l1) == id(l3))
+#l1.append(10)
+#print(l3)
+#l1.clear()
+#print(l1)
+#print(l3)
+#print(l4)
+#l1.extend(l2)
+#l1.remove(2)
+#l1.clear()
+
+lista = [1,2,3,4,5]
+elimina = [2,3]
+def eliminaElementos(lista, elimina):
+    n = len(lista)
+    i = 0
+    while i < n:
+        if lista[i] in elimina:
+            lista.pop(i)
+            n -= 1
+        else: i += 1
+    return lista
+lista = eliminaElementos(lista, elimina)
+print("ELIMINADOS")
+print(lista)
